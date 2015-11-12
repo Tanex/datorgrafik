0b04bc1ae5c94fab042e6fce216680ad829c82f2
Victor Hedlund <hedlund.victor@gmail.com>
2015-11-12 17:26:35 +0100

    as

--- labb2/x64/Release/fragmentShader.fs
+++ labb2/x64/Release/fragmentShader.fs
@@ -15,27 +15,40 @@ uniform vec4 light0pos;
 uniform sampler2D diffuseTexture;
 uniform sampler2D specularTexture;
 
-const vec3 Ca = {0.8, 0.8, 0.5};
-const vec3 Cd = {0.8, 0.8, 0.5};
-const vec3 Cs = {1.0, 1.0, 1.0};
-const vec3 Ia = {0.2, 0.2, 0.2};
-const vec3 Id = {0.8, 0.8, 0.8};
-const vec3 Is = {3.0, 3.0, 3.0};
-const float f = 20.0; 
+const vec4 Ca = vec4(0.8, 0.8, 0.5, 0.0);
+//const vec4 Cd = vec4(0.8, 0.8, 0.5, 0.0);
+//const vec4 Cs = vec4(1.0, 1.0, 1.0, 0.0);
+const vec4 Ia = vec4(0.2, 0.2, 0.2, 0.0);
+const vec4 Id = vec4(0.8, 0.8, 0.8, 0.0);
+const vec4 Is = vec4(3.0, 3.0, 3.0, 0.0);
+const float f = 80.0; 
 
 void main() {
-    //vec4 Cd = texture(diffuseTexture, textureCoordinate);
-    //vec4 Cs = texture(specularTexture, textureCoordinate);
-
+    vec4 Cd = texture(diffuseTexture, textureCoordinate);
+    vec4 Cs = texture(specularTexture, textureCoordinate);
+    
     vec4 wi = normalize(light0pos - position);
     vec4 n = normalize(normal);
-    vec4 wr = reflect(wi, n);//2*dot(wi,n)*n-wi;
-    vec4 v = normalize(position);
-    vec4 h = (wi+v)/length(wi+v);
 
-    //fragmentColour = Ca*Ia + Cd*Id*dot(wi, n) + Cs*Is * pow(dot(h,n), f);
-    fragmentColour.xyz = Ca*Ia + Cd*Id*dot(wi, n) + Cs*Is * pow(dot(h, n), f);
-    fragmentColour.a = 0.0;
+    float diffuse = max(dot(wi, n), 0.0);
+    float specular = 0.0;
+
+    if (diffuse > 0.0)
+    {        
+        vec4 v = normalize(position);
+
+        //Blinn-Phong
+        vec4 h = normalize(wi-v);
+        specular = pow(max(dot(h, n), 0.0), f);
+        
+        //Phong
+//        vec4 wr = reflect(wi, n);//2*dot(wi,n)*n-wi;
+//        specular = pow(max(dot(wr, v), 0.0), f/4.0);
+    }
+
+    fragmentColour = Ca*Ia + Cd*Id*diffuse + Cs*Is*specular;
+    //fragmentColour.xyz = Ca*Ia + Cd*Id*dot(wi, n) + Cs*Is * pow(max(dot(wr, v), 0.0), f);
+
 
     //fragmentColour = colour;
 }
